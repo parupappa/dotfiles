12d4106e35a86806e815b3f710029b6131fe7d6e
annosuke.yokoo <annosuke.yokoo@datadoghq.com>
2024-12-10 13:22:07 +0900

    update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,7 +4,8 @@ units = {
   right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
   left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
   top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
-  bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 }
+  bot50         = { x = 0.00, y = 0.50, w = 1.00, h = 0.50 },
+  center        = { x = 0.25, y = 0.25, w = 0.50, h = 0.50 }  -- 中央寄せ
 }
 
 mash = { 'option', 'ctrl' }
@@ -12,6 +13,8 @@ hs.hotkey.bind(mash, 'right', function() hs.window.focusedWindow():move(units.ri
 hs.hotkey.bind(mash, 'left', function() hs.window.focusedWindow():move(units.left50, nil, true) end)
 hs.hotkey.bind(mash, 'up', function() hs.window.focusedWindow():move(units.top50, nil, true) end)
 hs.hotkey.bind(mash, 'down', function() hs.window.focusedWindow():move(units.bot50, nil, true) end)
+-- 中央寄せのホットキー設定
+hs.hotkey.bind(mash, 'c', function() hs.window.focusedWindow():move(units.center, nil, true) end)
 
 -- Reload Confingの設定（これをしないと設定が反映されない）
 -- アプリからReload Confingを押しても良い
