ecb6a69acc93b24b172724d6d78abf4da99592fa
annosuke.yokoo <annosuke.yokoo@datadoghq.com>
2025-01-24 15:34:38 +0900

    update

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,16 +3,16 @@ hs.window.animationDuration = 0
 units = {
   right50       = { x = 0.50, y = 0.00, w = 0.50, h = 1.00 },
   left50        = { x = 0.00, y = 0.00, w = 0.50, h = 1.00 },
-  top50         = { x = 0.00, y = 0.00, w = 1.00, h = 0.50 },
-  bot65         = { x = 0.00, y = 0.65, w = 1.00, h = 0.65 },
+  top65         = { x = 0.00, y = 0.00, w = 1.00, h = 0.65 },
+  bot75         = { x = 0.00, y = 0.75, w = 1.00, h = 0.75 },
   center        = { x = 0.25, y = 0.25, w = 1.00, h = 0.50 }  -- 中央寄せ
 }
 
 mash = { 'option', 'ctrl' }
 hs.hotkey.bind(mash, 'right', function() hs.window.focusedWindow():move(units.right50, nil, true) end)
 hs.hotkey.bind(mash, 'left', function() hs.window.focusedWindow():move(units.left50, nil, true) end)
-hs.hotkey.bind(mash, 'up', function() hs.window.focusedWindow():move(units.top50, nil, true) end)
-hs.hotkey.bind(mash, 'down', function() hs.window.focusedWindow():move(units.bot65, nil, true) end)
+hs.hotkey.bind(mash, 'up', function() hs.window.focusedWindow():move(units.top65, nil, true) end)
+hs.hotkey.bind(mash, 'down', function() hs.window.focusedWindow():move(units.bot75, nil, true) end)
 -- 中央寄せのホットキー設定
 hs.hotkey.bind(mash, 'c', function() hs.window.focusedWindow():move(units.center, nil, true) end)
 
